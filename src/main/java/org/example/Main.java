7ba099a442d64ac78ab8bb4d89b1504a89f3553f
Andrey-m01 <andrey-m1@bigmir.net>
2023-04-16 01:46:00 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.io.*;
+
+
+import static org.example.utils.FileHandler.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Task one:\n");
+        File file = new File(".\\src\\main\\FileForTaskOne.txt");
+        phoneFinderFromFile(file);
+
+        System.out.println("\nTask two:\n");
+        File fileTwo = new File(".\\src\\main\\FileForTaskTwo.txt");
+        File fileForWrite = new File(".\\src\\main\\user.json");
+        readUserToJson(fileTwo, fileForWrite);
+
+        System.out.println("\nTask three:\n");
+        File fileForTaskThree = new File(".\\src\\main\\words.txt");
+        frequencyCalculator(fileForTaskThree);
+
+    }
+}
