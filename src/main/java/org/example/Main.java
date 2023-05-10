1182e7e6a1e9c70c69cb243b07444df0663bc6ce
Andrey-m01 <andrey-m1@bigmir.net>
2023-05-10 03:39:32 +0300

    Multiplatform path used

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,16 +9,16 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Task one:\n");
-        File file = new File(".\\src\\main\\FileForTaskOne.txt");
+        File file = new File("./src/main/FileForTaskOne.txt");
         phoneFinderFromFile(file);
 
         System.out.println("\nTask two:\n");
-        File fileTwo = new File(".\\src\\main\\FileForTaskTwo.txt");
-        File fileForWrite = new File(".\\src\\main\\user.json");
+        File fileTwo = new File("./src/main/FileForTaskTwo.txt");
+        File fileForWrite = new File("./src/main/user.json");
         readUserToJson(fileTwo, fileForWrite);
 
         System.out.println("\nTask three:\n");
-        File fileForTaskThree = new File(".\\src\\main\\words.txt");
+        File fileForTaskThree = new File("./src/main/words.txt");
         frequencyCalculator(fileForTaskThree);
 
     }
